ceff0f52198ef78e076aeaa619404d508fc989df
Josimar Souza <contact.josimarsouza@gmail.com>
2025-03-26 15:05:37 -0300

    implementing the iss context

--- src/context/issContext/index.tsx
+++ src/context/issContext/index.tsx
@@ -1,13 +1,49 @@
-import { createContext } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
+import { IssInfos } from "../../services/api/types";
+import IssTrackerAPI from "../../services/api";
 
 type IssContextProps = {
   children: React.ReactNode,
 }
 
 const issContext = createContext({});
+const issTrackerAPI = new IssTrackerAPI(5000);
 
 function IssContext({ children }: IssContextProps) {
-  const providerValues = {};
+  const [ issInfos, setIssInfos ] = useState<IssInfos>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchIssData = async () => {
+      setIsLoading(true);
+      const issData: IssInfos | undefined = await issTrackerAPI.getIss();
+
+      if (issData != undefined) {
+        setIssInfos(issData);
+        setIsLoading(false);
+      }
+    };
+
+    fetchIssData();
+
+    const issDataIntervalID = setInterval(() => {
+      fetchIssData();
+    }, 3000);
+
+    return () => {
+      clearInterval(issDataIntervalID);
+    }
+  }, []);
+
+  const providerValues = useMemo(() => ({
+    issInfos,
+    isLoading,
+  }), [isLoading, issInfos]);
 
   return (
     <issContext.Provider
